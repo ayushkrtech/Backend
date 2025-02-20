8b5abd6897341cc75c922951be5018e08f812949
Ayush <techarunprd@gmail.com>
2025-02-20 22:15:46 +0530

    Create 02_script.js

---
+++ basics /02_script.js
@@ -0,0 +1,11 @@
+I will Update If I learn more about this by time
+
+
+
+// const http = require('http');
+
+// const server = http.createServer(function(req,res){
+//     res.end("hello world")
+// })
+
+// server.listen(3000);
